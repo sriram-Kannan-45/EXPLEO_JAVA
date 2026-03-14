71dfb83983c5f47b48a6359cd4726058a7281601
SRIRAM <wavene20@gmail.com>
2026-03-14 10:32:55 +0530

    14/3/2026

--- HANDSON_9/hard/Q1.java
+++ HANDSON_9/hard/Q1.java
@@ -1,38 +1,38 @@
-package hard;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-public class Q1
-{
-	public static void main (String srgs[])
-	{
-		Path p = Paths.get("D:\\\\drive\\\\out\\\\production\\\\section_1\\\\MainTxt.txt");
-		Path slp=Paths.get("D:\\drive\\section_1\\src\\p_1.java");
-		boolean resut=Files.exists(p);
-		
-		System.out.print("Path: "+p+" File Exists: "+res);
-		
-		if(resut)
-		{
-			try
-			{
-				Files.createSymbolicLink(slp,p);
-				
-			}
-			catch(IOException x)
-			{
-				System.out.println(x);
-			}
-			System.out.println();
-			boolean readable=Files.isReadable(p);
-			System.out.println("Readable: "+readable);
-			
-			boolean writable=Files.isWritable(p);
-			System.out.println("Writable: "+writable);
-			
-		}
-	}
-}
+//package hard;
+//
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
+//
+//public class Q1
+//{
+//	public static void main (String srgs[])
+//	{
+//		Path p = Paths.get("D:\\\\drive\\\\out\\\\production\\\\section_1\\\\MainTxt.txt");
+//		Path slp=Paths.get("D:\\drive\\section_1\\src\\p_1.java");
+//		boolean resut=Files.exists(p);
+//		
+//		System.out.print("Path: "+p+" File Exists: "+res);
+//		
+//		if(resut)
+//		{
+//			try
+//			{
+//				Files.createSymbolicLink(slp,p);
+//				
+//			}
+//			catch(IOException x)
+//			{
+//				System.out.println(x);
+//			}
+//			System.out.println();
+//			boolean readable=Files.isReadable(p);
+//			System.out.println("Readable: "+readable);
+//			
+//			boolean writable=Files.isWritable(p);
+//			System.out.println("Writable: "+writable);
+//			
+//		}
+//	}
+//}
--- HANDSON_9/simple/Q1.java
+++ HANDSON_9/simple/Q1.java
@@ -1,17 +1,17 @@
-package simple;
-
-import java.io.File;
-
-public class Q1 {
-
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		
-		File file = new File ("D:\\drive\\out\\production\\section_1\\MainTxt.txt");
-		
-		System.out.println(file.);
-
-	}
-
-}
+//package simple;
+//
+//import java.io.File;
+//
+//public class Q1 {
+//
+//	public static void main(String[] args) 
+//	{
+//		// TODO Auto-generated method stub
+//		
+//		File file = new File ("D:\\drive\\out\\production\\section_1\\MainTxt.txt");
+//		
+//		System.out.println(file.);
+//
+//	}
+//
+//}
