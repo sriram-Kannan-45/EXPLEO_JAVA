10e6867989f4902133c5d2897533305e7246f614
SRIRAM <wavene20@gmail.com>
2026-03-16 11:45:33 +0530

    16/3/2026

---
+++ src/array/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("hii");
+
+	}
+
+}
---
+++ src/collections/ArrayList1.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+	public static void main(String[] args)
+
+	{
+		// TODO Auto-generated method stub
+
+		ArrayList<String> arr = new ArrayList<>();
+
+		System.out.println("initial Array Size : ");
+
+		arr.add("C");
+
+		arr.add("A");
+
+		arr.add("E");
+
+		arr.add("B");
+
+		arr.add("D");
+
+		arr.add("F");
+
+		arr.add(1, "G");
+
+		System.out.println("After adding some data : ");
+
+	}
+}
